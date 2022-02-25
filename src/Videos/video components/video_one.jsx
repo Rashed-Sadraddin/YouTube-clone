c0591b392262eae8b6ed5fb20aeb82753fe39c76
Rashed-Sadraddin <96119144+Rashed-Sadraddin@users.noreply.github.com>
2022-02-25 23:20:38 +0300

    v8

--- src/Videos/video components/video_one.jsx
+++ src/Videos/video components/video_one.jsx
@@ -1,8 +1,70 @@
+import Video1 from "../a.mp4";
+import Videos from "../Videos";
+import { NavLink } from "react-router-dom";
+import { CheckCircleFilled, EllipsisOutlined } from "@ant-design/icons";
+import { ReactComponent as LIKE } from "./Video Tools/LIKE.svg";
+import { ReactComponent as LIKE2 } from "./Video Tools/LIKE2.svg";
+import { ReactComponent as DISLIKE } from "./Video Tools/DISLIKE.svg";
+import { ReactComponent as DISLIKE2 } from "./Video Tools/DISLIKE2.svg";
+import { ReactComponent as SHARE } from "./Video Tools/SHARE.svg";
+import { ReactComponent as CLIP } from "./Video Tools/CLIP.svg";
+import { ReactComponent as SAVE } from "./Video Tools/SAVE.svg";
+
 
 const Video_one = () => {
   return (
-    <div className="text-white">video_one</div>
-  )
+    <div className="flex flex-row justify-between  overflow-y-auto w[100%] px-14 pt-6">
+      <div>
+        <div className="w-[716px] h-[403px]">
+         <video src={Video1} controls className="w-[100%] h-[100%]" /> 
+        </div>
+        
+        <h1 className="text-white mt-6">Title</h1>
+
+        <div className="flex flex-row justify-between">
+          <span className="text-neutral-400">view . time </span>
+
+        <div className="flex flex-row">
+        <button className="text-white mx-3 flex flex-row"><LIKE className="w-6 h-6 fill-white"/> &nbsp;LIKE</button>
+        {/* <button className="w-3"><LIKE2 className="w-6 h-6 fill-white"/></button> */}
+        <button className="text-white  mx-3 flex flex-row"><DISLIKE className="w-6 h-6 fill-white"/>&nbsp;DISLIKE</button>
+        {/* <button className="w-3"><DISLIKE2 className="w-6 h-6 fill-white"/></button> */}
+        <button className="text-white  mx-3 flex flex-row"><SHARE className="w-6 h-6 fill-white"/>&nbsp;SHARE</button>
+        <button className="text-white mx-3 flex flex-row"><CLIP className="w-6 h-6 fill-white"/>&nbsp;CLIP</button>
+        <button className="text-white  mx-3 flex flex-row"><SAVE className="w-6 h-6 fill-white"/>&nbsp;SAVE</button>
+        <button className="text-white mx-3 text-2xl flex items-center"><EllipsisOutlined/></button>
+          </div>
+        </div>
+        
+      </div>
+
+      <div className="flex flex-col pt-2 ml-6 w-[475px]">
+        {Videos.map((props) => {
+          return (
+            <NavLink to="/one">
+              <div className="group flex flex-row mr-2 my-1 h-[94px] w-[401] ">
+                <img className="w-[168px] pr-2" src={props.link}></img>
+          <div className=" w-[200px] flex flex-col text-white">
+          
+          
+            <h4 className="text-white">{props.title} </h4>
+            <p className="text-neutral-400 mb-0">{props.pagename} <CheckCircleFilled className=""/></p>
+            <span className="text-neutral-400">{props.view} views . {props.time} ago</span>
+          </div>
+          
+          <div className="text-neutral-900 group-hover:text-white">
+              <button>
+          <EllipsisOutlined rotate={90} className=" text-lg h-4 w-4"/>
+          </button>
+          </div>
+
+              </div>
+            </NavLink>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Video_one
--- src/sidebar/components/Home.jsx
+++ src/sidebar/components/Home.jsx
@@ -3,19 +3,21 @@ import { CheckCircleFilled, EllipsisOutlined ,
   ClockCircleOutlined,MenuUnfoldOutlined} from "@ant-design/icons";
 import Links from "../Link components/Links/Links";
 import { NavLink } from "react-router-dom";
+import "../../App.css";
 
 const Home =()=>{return (
-  <div>
+  <div className="">
       <div className="flex flex-row bg-neutral-900 h-full">
         <Links /> 
-  <div className="  flex flex-row flex-wrap w-[94%] h-[100%] overflow-scroll px-4 ">
+  <div className=" flex flex-row flex-wrap w-[94%] h-[100%] overflow-y-auto  px-4 ">
     {Videos.map((props) => {
       return (
+        <div className="static w-[285px] h-[280px]">
         <NavLink to="/one">
         <div 
-        className="hover:bg-neutral-800 mx-2 my-6  w-[270px] hover:scale-[1.3] hover:duration-[0.5s] hover:delay-[1s]">
+        className="hover:relative group hover:bg-neutral-800 mx-2 my-6  w-[270px] hover:scale-[1.3] hover:duration-[0.5s] hover:delay-[1s] h-[240px] hover:h-[330px] overflow-hidden">
           <img className="mb-3" src={props.link}></img>
-          <div className="hover: hover:pr-2 hover:pl-2 flex  text-white">
+          <div className=" group-hover:pr-2 group-hover:pl-2 group-hover:delay-[1s] group-hover:duration-[0.5s] flex  text-white">
           <img className="float-left w-10 h-10 rounded-full mr-3" src={props.pagepic}></img>
           <div className="float-left w-[70%] ">
             <h4 className="text-white w-[160px] float-left">{props.title} </h4>
@@ -28,14 +30,14 @@ const Home =()=>{return (
           </button>
           </div>
           </div>
-          {/* <button className="text-white bg-neutral-700 rounded-[2px] flex justify-center items-center w-[95%] p-2 mx-auto mb-1">
+          <button className="text-white bg-neutral-700 rounded-[2px] flex justify-center items-center w-[95%] p-2 mx-auto my-1">
             <ClockCircleOutlined className="text-[20px]" /> &nbsp; WATCH LATER
             </button>
             <button className="text-white bg-neutral-700 flex justify-center rounded-[2px] w-[95%] p-2 mx-auto">
               <MenuUnfoldOutlined className="text-[20px]"/>&nbsp;ADD TO QUEUE
-              </button> */}
+              </button>
           
-        </div></NavLink>
+        </div></NavLink></div>
       );
     })}
   </div>
